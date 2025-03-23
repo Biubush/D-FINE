5a870c59b861283bc1ee6474a57e93726b641421
biubush <1577141450@qq.com>
2025-03-23 15:00:44 +0800

    完善

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Set WshShell = CreateObject("WScript.Shell") > %temp%\run_hidden.vbs
-echo WshShell.Run "cmd /c cd D:\Biubush\Archives\CodeSpace\D-FINE && D:\Biubush\Archives\CodeSpace\D-FINE\.conda\python.exe D:\Biubush\Archives\CodeSpace\D-FINE\early_stop.py -c D:\Biubush\Archives\CodeSpace\D-FINE\configs\dfine\custom\sar_dfine_s.yml --use-amp --seed=0 --patience=50 --min-epochs=100", 0, false >> %temp%\run_hidden.vbs
-wscript %temp%\run_hidden.vbs
